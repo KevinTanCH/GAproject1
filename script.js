335b0ce146fca49b86fc0c6a27e3084b01405dcb
kevintch <KevinTanChayHan@hotmail.com>
2024-02-25 18:44:02 +0800

    added game over

--- script.js
+++ script.js
@@ -84,22 +84,43 @@ class Monster extends Character {
 // Monster types here
 const arrMonstSlime = [25, 25, 1, "Slime", 1];
 const arrMonstSkele = [50, 50, 2, "Skeleton", 1];
-const arrMonstMino = [100, 100, 3, "Minotaur", 3];
-const arrMonstCyclo = [200, 200, 5, "Cyclops", 10];
-const arrMonstList = [
+const arrMonstCent = [150, 150, 3, "Centaur", 3];
+const arrMonstMant = [200, 200, 3, "Manticore", 3];
+const arrMonstChim = [200, 200, 3, "Chimera", 3];
+const arrMonstMino = [200, 200, 3, "Minotaur", 3];
+const arrMonstCyclo = [400, 400, 5, "Cyclops", 10];
+const arrMonstHydra = [500, 500, 5, "Hydra", 10];
+const arrMonstColo = [500, 500, 5, "Colossus", 10];
+const arrMonstAllList = [
   arrMonstSlime,
   arrMonstSkele,
+  arrMonstCent,
+  arrMonstMant,
+  arrMonstChim,
   arrMonstMino,
   arrMonstCyclo,
+  arrMonstHydra,
+  arrMonstColo,
 ];
 // Global Variables
-let boolDebugMode = false;
+let boolDebugMode = true;
 let intCoins = 10;
 let intDangerLevel = 0;
 let intTownModeCounter = 0;
 const kvpConsumables = { potsHP: 0 };
-const kvpWeapons = { bow: 0 };
-const kvpArmour = { Gambeson: 0, Chainmail: 0, Plate: 0 };
+const kvpWeapons = {
+  Bow: 0,
+  Spear: 0,
+  BattletAxe: 0,
+  WarHammer: 0,
+  GreatSword: 0,
+  Sword: 0,
+  Rapier: 0,
+  Axe: 0,
+  Mace: 0,
+  Shield: 0,
+};
+const kvpArmour = { Gambeson: 0, Chainmail: 0, Platemail: 0, Plate: 0 };
 let intMonsterTurnBaseDamage = 0;
 let intHeroTurnBaseDamage = 0;
 let boolCombatEnd = true;
@@ -109,32 +130,64 @@ let boolGameContinue = true;
 const hero1 = new Hero(100, 100, 1, "Adam", 2);
 const monster1 = new Monster(...arrMonstSlime);
 
-/*----- event listeners -----*/
+/*----- Event listeners -----*/
 // CombatButtons
 document.querySelector(".Button1").addEventListener("click", fnHeroTurn);
-document.querySelector(".Button2").addEventListener("click", fnPotionHeal);
-document.querySelector(".Button3").addEventListener("click", fnHideElements);
-document.querySelector(".Button4").addEventListener("click", fnShowElements);
+document.querySelector(".Button2").addEventListener("click", fnHeroTurn);
+document.querySelector(".Button3").addEventListener("click", fnPotionHeal);
+document.querySelector(".Button4").addEventListener("click", fnRunaway);
 //ContinueButtons
-document.querySelector(".BtnToTown").addEventListener("click", fnStartTown);
+document.querySelector(".BtnToTown").addEventListener("click", fnContToTown);
 document
   .querySelector(".BtnContinuePotionHeal")
-  .addEventListener("click", fnPotionHeal);
+  .addEventListener("click", fnContPotionHeal);
 document
   .querySelector(".BtnContinueCombat")
-  .addEventListener("click", fnStartCombat);
+  .addEventListener("click", fnContToCmbt);
 // TownButtons
 document.querySelector(".BtnBuyPotsHP").addEventListener("click", fnBuyItems);
 document.querySelector(".BtnTownHeal").addEventListener("click", fnTownHeal);
 document.querySelector(".BtnLevelUp").addEventListener("click", fnTownLevelUp);
 document
-  .querySelector(".BtnStartCombat")
-  .addEventListener("click", fnStartCombat);
+  .querySelector(".BtnTownToCombat")
+  .addEventListener("click", fnTownToCombat);
 // StartMenuButtons
 document.querySelector(".BtnStartGame").addEventListener("click", fnStartGame);
 document.querySelector(".BtnDebugGame").addEventListener("click", fnDebugMode);
+// GameOverButtons
+// document.querySelector(".BtnRestart").addEventListener("click", fnStartGame);
 
-/*----- functions -----*/
+/*----- Functions -----*/
+// Hide and Show UI, control player button choices
+function fnUIStartToTown() {
+  fnHideElements("StartMenuUI");
+  fnShowElements("TownModeUI");
+  return;
+}
+function fnUITownToCmbt() {
+  fnHideElements("TownModeUI");
+  fnShowElements("CombatModeUI");
+}
+function fnUICmbtToCont() {
+  fnHideElements("CombatModeUI");
+  fnShowElements("ContinueMenuUI");
+  return;
+}
+function fnUIContToCmbt() {
+  fnHideElements("ContinueMenuUI");
+  fnShowElements("CombatModeUI");
+  return;
+}
+function fnUIContToTown() {
+  fnHideElements("ContinueMenuUI");
+  fnShowElements("TownModeUI");
+}
+function fnUIGameOver() {
+  fnHideElements("CombatModeUI");
+  fnShowElements("GameOverUI");
+}
+
+// Combat Mode
 function fnMonsterTurn() {
   console.log("Monster Turn");
   if (fnCheckCombatEnd()) {
@@ -145,6 +198,9 @@ function fnMonsterTurn() {
   hero1.damage(intMonsterTurnBaseDamage);
   console.log(hero1);
   console.log(monster1);
+  if (fnCheckCombatEnd()) {
+    return;
+  }
   return;
 }
 function fnHeroTurn() {
@@ -165,16 +221,17 @@ function fnHeroTurn() {
 }
 function fnCheckCombatEnd() {
   if (monster1.hPoints < 1) {
-    boolGameContinue = false;
+    boolGameContinue = true;
     boolCombatEnd = false;
     intCoins += 3 * intDangerLevel;
     console.log("More Coins: " + intCoins);
-    fnContinueMode();
+    fnUICmbtToCont();
     return true;
   } else if (hero1.hPoints < 1) {
     boolGameContinue = false;
     boolCombatEnd = false;
     console.log("Game over");
+    fnUIGameOver();
     return true;
   } else {
     return false;
@@ -184,26 +241,77 @@ function fnPotionHeal() {
   console.log("Potion Heal");
   if (kvpConsumables.potsHP > 0) {
     hero1.heal(50);
+    console.log(hero1);
+    fnMonsterTurn();
     return;
+  } else {
+    console.log("No more potions");
   }
   return;
 }
+function fnRunaway() {
+  if (Math.random() > 0.5) {
+    console.log("Tactical retreat success");
+    fnTownMode();
+    return;
+  } else {
+    console.log("Tactical retreat failed");
+    fnMonsterTurn();
+    return;
+  }
+}
 
-function fnContinueMode() {
-  fnHideElements("CombatModeUI");
-  fnShowElements("ContinueMenuUI");
+// Continue mode
+function fnContToTown() {
+  fnTownMode();
+}
+function fnContPotionHeal() {
+  console.log("Potion Heal");
+  if (kvpConsumables.potsHP > 0) {
+    hero1.heal(50);
+    console.log(hero1);
+    return;
+  } else {
+    console.log("No more potions");
+  }
+  return;
+}
+function fnContToCmbt() {
+  console.log("Combat start");
+  fnUIContToCmbt();
+  boolCombatEnd = false;
+  intDangerLevel++;
+  console.log("Danger Level: " + intDangerLevel);
+  if (intDangerLevel < 5) {
+    monster1.changeMonster(arrMonstAllList[intDangerLevel - 1]);
+  } else {
+    monster1.changeMonster(arrMonstCyclo);
+  }
+  monster1.heal(monster1.maxHP);
+  fnMonsterTurn();
   return;
 }
 
-function fnStartTown() {
+// Town mode
+function fnTownMode() {
   console.log("Combat Ends, start town");
+  fnUIContToTown();
   boolCombatEnd = true;
   intDangerLevel = 0;
   console.log("Danger Level: " + intDangerLevel);
-  fnHideElements("StartMenuUI");
-  fnHideElements("ContinueMenuUI");
-  fnShowElements("TownModeUI");
   return;
+}
+function fnBuyItems() {
+  if (intCoins < potionHP.cost) {
+    console.log("Not enough coins!");
+    return;
+  } else {
+    intCoins = intCoins - potionHP.cost;
+    kvpConsumables.potsHP++;
+    console.log(kvpConsumables);
+    console.log("Coins: " + intCoins);
+    return;
+  }
 }
 function fnTownHeal() {
   console.log("Town heal");
@@ -216,6 +324,7 @@ function fnTownHeal() {
   } else {
     intCoins--;
     hero1.heal(50);
+    console.log(hero1);
     console.log("Coins: " + intCoins);
     return;
   }
@@ -235,41 +344,31 @@ function fnTownLevelUp() {
     return;
   }
 }
-function fnBuyItems() {
-  if (intCoins < potionHP.cost) {
-    console.log("Not enough coins!");
-    return;
-  } else {
-    intCoins = intCoins - potionHP.cost;
-    kvpConsumables.potsHP++;
-    console.log(kvpConsumables);
-    console.log("Coins: " + intCoins);
-    return;
-  }
-}
-function fnStartCombat() {
+function fnTownToCombat() {
   console.log("Combat start");
-  fnHideElements("TownModeUI");
-  fnShowElements("CombatModeUI");
+  fnUITownToCmbt();
   boolCombatEnd = false;
   intDangerLevel++;
   console.log("Danger Level: " + intDangerLevel);
   if (intDangerLevel < 5) {
-    monster1.changeMonster(arrMonstList[intDangerLevel - 1]);
+    monster1.changeMonster(arrMonstAllList[intDangerLevel - 1]);
   } else {
     monster1.changeMonster(arrMonstCyclo);
   }
   monster1.heal(monster1.maxHP);
+  console.log(monster1);
   fnMonsterTurn();
   return;
 }
+
+// Start mode
 function fnStartGame() {
-  console.log("Start town");
+  console.log("Start game, start town");
+  fnUIStartToTown();
   boolCombatEnd = true;
   intDangerLevel = 0;
   console.log("Danger Level: " + intDangerLevel);
-  fnHideElements("StartMenuUI");
-  fnShowElements("TownModeUI");
+
   return;
 }
 
@@ -281,22 +380,13 @@ function initialize() {
   console.log("Game Loaded");
   fnShowElements("StartMenuUI");
   if (boolDebugMode) {
-    fnShowElements("CombatModeUI");
-    fnShowElements("ContinueMenuUI");
-    fnShowElements("TownModeUI");
+    fnDebugMode();
   }
 }
 initialize();
-// window.onload = function () {
-//   if (localStorage.getItem("hasCodeRunBefore") === null) {
-//     /** Your code here. **/
-
-//     localStorage.setItem("hasCodeRunBefore", true);
-//   }
-// };
 
 function fnDebugMode() {
-  // Reveals UI
+  // Reveals all UI with debug button
   boolDebugMode = true;
   let elements = document.getElementsByClassName("StartMenuUI");
   for (let i = 0; i < elements.length; i++) {
@@ -311,6 +401,10 @@ function fnDebugMode() {
     elements[i].classList.remove("hidden");
   }
   elements = document.getElementsByClassName("CombatModeUI");
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].classList.remove("hidden");
+  }
+  elements = document.getElementsByClassName("GameOverUI");
   for (let i = 0; i < elements.length; i++) {
     elements[i].classList.remove("hidden");
   }
